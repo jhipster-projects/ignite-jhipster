be7c0074ab7c43b24c8813a2bee310ef1a7b9382
Jonathan Ruddell <jonathan.ruddell@gmail.com>
2020-05-19 16:02:14 -0400

    fix e2e

--- test/scripts/install-node-dependencies.sh
+++ test/scripts/install-node-dependencies.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 set -e
 
-npm install -g generator-jhipster git://github.com/ruddell/ignite.git#add-skip-install-support --ignore-scripts spawn-sync
+npm install -g generator-jhipster --ignore-scripts spawn-sync
+
+git clone --single-branch --branch add-skip-install-support https://github.com/ruddell/ignite.git
+pushd ignite
+npm link
+popd
 
 # set yarn network-timeout to 10 minutes
 yarn config set network-timeout 600000
